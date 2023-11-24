f8f61fe2b8669301affad3f8579efb7340ee8552
georgeWasfy <g.a.wasfy@gmail.com>
2023-11-24 19:51:53 +0200

    scafold apps

---
+++ apps/main-be-app/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'main-be-app',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/main-be-app',
+};
