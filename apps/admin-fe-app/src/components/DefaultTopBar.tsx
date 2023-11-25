6a9c542ed4dd5aa15e113beafbb74915f452fb96
georgeWasfy <g.a.wasfy@gmail.com>
2023-11-25 13:48:54 +0200

    avatar component

--- apps/admin-fe-app/src/components/DefaultTopBar.tsx
+++ apps/admin-fe-app/src/components/DefaultTopBar.tsx
@@ -1,10 +1,15 @@
+import { Avatar, AvatarFallback, AvatarImage } from '@engine/design-system';
 import ThemeSwitcher from './ThemeSwitcher';
 interface TopBarProps extends React.HTMLAttributes<HTMLDivElement> {
   switchTheme: (s: string) => void;
 }
 const DefaultTopBar = ({ switchTheme }: TopBarProps) => {
   return (
-    <nav>
+    <nav className='flex'>
+      <Avatar>
+        <AvatarImage src="" />
+        <AvatarFallback>User</AvatarFallback>
+      </Avatar>
       <ThemeSwitcher switchTheme={switchTheme} />
     </nav>
   );
--- libs/design-system/src/index.ts
+++ libs/design-system/src/index.ts
@@ -1,4 +1,6 @@
 export * from './lib/Button';
 export * from './lib/utils/helpers'
 export * from './lib/Tabs'
+export * from './lib/Avatar'
+
 
